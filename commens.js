73b26a51d0e015d7ff58939ee9552841d43bac92
PressStart10 <73621363+PressStart10@users.noreply.github.com>
2020-10-29 15:31:50 -0400

    Update commens.js

--- commens.js
+++ commens.js
@@ -1,8 +1,9 @@
 var ajax = ""
 function l(li){
-  function print(in){
+  function log(in){
     ajax += in
     $("#cmd").html(ajax+"<br>");
+    consle log("ajax")
   }
   $("#rf").value("line input..")
 }
